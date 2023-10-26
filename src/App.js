746e1d2f88534e5d2f3068c439cbe7fc15816b05
Chidinma <Mysticchina@gmail.com+odin@users.noreply.github.com>
2023-10-26 14:44:52 +0100

    cleared app.js to remove git conflicts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './pages/Login';
-import SignUp from './pages/Signup';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path="/signup" element={<SignUp />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/" element={<SignUp />}/>
-        </Routes>
+        
     </div>
   );
 }
