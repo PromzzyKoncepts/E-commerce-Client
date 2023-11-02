07866dbc58a8be521fa09854fde39b49b882415e
Chidi <chidiebubeokoye755@gmail.com>
2023-11-02 18:33:48 +0100

    created UI for EmailVerf for the component

--- src/Pages/EmailVerf.jsx
+++ src/Pages/EmailVerf.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const VerifyPage = () => {
+  const [email, setEmail] = useState("");
+  const [verificationResult, setVerificationResult] = useState(null);
+
+  
+  const handleEmailChange = (e) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+    localStorage.setItem('email', newEmail);
+  };
+
+  const userEmail = localStorage.getItem("email")
+  console.log(userEmail)
+
+  const handleVerifyClick = () => {
+    const baseUrl = 'https://aphia-dev.onrender.com/api/users/resend'; 
+
+    axios
+      .post(baseUrl, { userEmail })
+      .then(response => {
+        console.log(response)
+        // setVerificationResult(response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+
   return (
     <div className="text-center fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
       <div className=" p-[5rem] bg-white rounded-lg shadow-lg">
@@ -10,12 +39,19 @@ const VerifyPage = () => {
           Kindly check your mail inbox or spam to verify your account before proceeding.
         </p>
         <p className='font-bold'>Didn't get a mail...? </p>
-        <button className="mt-6 bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        <button className="mt-6 bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={handleVerifyClick}>
           Resend 
         </button>
-      </div>
+        {/* {verificationResult && (
+          <p className="mt-4 text-green-900">
+            Verification Result: {verificationResult}
+          </p>
+        )} */}
+        </div>
     </div>
   );
 };
+
 
 export default VerifyPage;
