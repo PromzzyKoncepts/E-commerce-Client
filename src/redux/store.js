7b849e4e604dca26507077046062ca9ce49150ab
Ipheco <iamipheco@gmail.com>
2023-11-04 02:11:07 +0100

    i added all the slices i created to the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./features/cartSlice";
 import shippingAddressSlice from "./features/shippingAddressSlice";
+import userSlice from "./features/userSlice";
+import allOrdersSlice from "./features/allOrdersSlice";
+import allProductsSlice from "./features/allProductsSlice";
+import paymentsSlice from "./features/paymentsSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 
@@ -15,6 +19,10 @@ const persistConfig = {
 const allReducers = combineReducers({
     cart: cartSlice,
     shippingAddress: shippingAddressSlice,
+    user: userSlice,
+    order: allOrdersSlice,
+    product: allProductsSlice,
+    payment: paymentsSlice,
 })
 
 const persistedReducer = persistReducer(persistConfig, allReducers)
