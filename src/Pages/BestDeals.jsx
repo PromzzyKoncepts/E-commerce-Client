5bef289fefdc0e8e268fe7f718e1fef65a246bd3
Obiefuna Perpetua <obiefuna2017@gmail.com>
2023-11-05 20:11:40 +0100

    implement pagination

--- src/Pages/BestDeals.jsx
+++ src/Pages/BestDeals.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import sneakers from "../assets/brown sneakers.jpeg"
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 
 function BestDeals() {
 
@@ -16,15 +19,30 @@ function BestDeals() {
         category: "shoes",
         price: "$20",
     };
-    const products = new Array(50).fill(productObject);
+
+    const [currentPage, setCurrentPage] = useState(1)
+    let totalPages = useRef()
+    const itemsPerPage = 15
+    const startIndex = (currentPage - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+    
+    const products = new Array(50).fill(productObject)
+    totalPages.current = Math.ceil(products.length/itemsPerPage)
+    const productsToDisplay = products.slice(startIndex, endIndex)
+
+    const handlePrev = ()=> setCurrentPage((prev)=>prev-1)
+    
+
+    const handleNext = ()=> setCurrentPage((prev)=>prev+1)
+    
     return(
         <div className=''>
             <div className="w-10/12 mx-auto mt-4 bg-slate-200 pt-6 p-4 rounded-t-lg relative">
                 <div className="">
                     <h2 className='pb-4 text-3xl mx-auto w-fit mb-4'>Best Deals</h2>
-                    <div className='grid grid-cols-4 gap-3 '>
+                    <div className='grid grid-cols-5 gap-3 '>
                         {
-                            products.map(item => (
+                            productsToDisplay.map(item => (
                                 <a href={productObject.href} key={productObject.id} className='bg-white pb-2 px-6 pt-6 rounded relative no-underline'>
                                     <div className='bg-white'>
                                         <small className='text-slate-100 bg-red-500 p-1 absolute top-0 left-0 rounded-tl-sm'>{productObject.type}</small>
@@ -52,6 +70,10 @@ function BestDeals() {
                                 </a>
                             ))
                         }
+                    </div>
+                    <div className='mx-auto w-fit flex gap-5 shadow-md p-2 mt-2 rounded-sm'>
+                        <button onClick={handlePrev} disabled={currentPage === 1} className='disabled:opacity-40'>{<ArrowBackIosIcon />}</button>
+                        <button onClick={handleNext} disabled={currentPage === totalPages.current} className='disabled:opacity-40'>{<ArrowForwardIosIcon />}</button>
                     </div>
                 </div>
             </div>
